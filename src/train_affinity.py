3a3fc814b02e5b3fdf3e67317dd25ba68cb650f5
Jackz915 <zhangjk9@mail2.sysu.edu.cn>
2024-07-03 15:52:09 +0800

    Create train_affinity.py

---
+++ src/train_affinity.py
@@ -0,0 +1,109 @@
+import os
+import hydra
+import rootutils
+import pytorch_lightning as pl
+from omegaconf import DictConfig
+from pytorch_lightning import Callback, LightningDataModule, LightningModule, Trainer
+from pytorch_lightning.loggers import Logger
+from typing import List, Optional, Tuple
+
+rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
+
+from src import utils
+# os.environ["PL_TORCH_DISTRIBUTED_BACKEND"] = "gloo"
+
+log = utils.get_pylogger(__name__)
+
+
+@utils.task_wrapper
+def train(cfg: DictConfig):
+    # Set up RNG and device
+    if cfg.get("seed"):
+        pl.seed_everything(cfg.seed, workers=True)
+
+    # Load data and get dataloaders
+    log.info(f"Instantiating datamodule <{cfg.datamodule._target_}>")
+    datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule)
+
+    log.info(f"Instantiating model <{cfg.model._target_}>")
+    model: LightningModule = hydra.utils.instantiate(
+        cfg.model
+    )
+
+    log.info("Instantiating callbacks...")
+    callbacks: List[Callback] = utils.instantiate_callbacks(
+        cfg.get("callbacks")
+    )
+
+    log.info("Instantiating loggers...")
+    logger: List[Logger] = utils.instantiate_loggers(
+        cfg.get("logger")
+    )
+
+    log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
+    trainer: Trainer = hydra.utils.instantiate(
+        cfg.trainer, callbacks=callbacks, logger=logger
+    )
+
+    object_dict = {
+        "cfg": cfg,
+        "datamodule": datamodule,
+        "model": model,
+        "callbacks": callbacks,
+        "logger": logger,
+        "trainer": trainer,
+    }
+
+    if logger:
+        log.info("Logging hyperparameters!")
+        utils.log_hyperparameters(object_dict)
+
+    if cfg.get("train"):
+        log.info("Starting training!")
+        ckpt_path = (
+            cfg.get("ckpt_path")
+            if cfg.get("ckpt_path") is not None and os.path.exists(cfg.get("ckpt_path"))
+            else None
+        )
+        trainer.fit(
+            model=model,
+            datamodule=datamodule,
+            ckpt_path=ckpt_path
+        )
+
+    train_metrics = trainer.callback_metrics
+
+    if cfg.get("test"):
+        log.info("Starting testing!")
+        ckpt_path = trainer.checkpoint_callback.best_model_path
+        if ckpt_path == "":
+            log.warning("Best ckpt not found! Using current weights for testing...")
+            ckpt_path = None
+        trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
+        log.info(f"Best ckpt path: {ckpt_path}")
+
+    test_metrics = trainer.callback_metrics
+
+    # merge train and test metrics
+    metric_dict = {**train_metrics, **test_metrics}
+
+    return metric_dict, object_dict
+
+
+@hydra.main(version_base="1.3", config_path="../configs", config_name="train_affinity.yaml")
+def main(cfg: DictConfig):
+    # train the model
+    metric_dict, _ = train(cfg)
+
+    # safely retrieve metric value for hydra-based hyperparameter optimization
+    metric_value = utils.get_metric_value(
+        metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
+    )
+
+    # return optimized metric
+    return metric_value
+
+
+if __name__ == '__main__':
+    protein = main()
+
