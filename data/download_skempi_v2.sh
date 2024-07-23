073a4012a0acc857bc04136c159aaa86fb1ff6dd
Jackz915 <zhangjk9@mail2.sysu.edu.cn>
2024-07-24 03:57:52 +0800

    Update download_skempi_v2.sh

--- data/download_skempi_v2.sh
+++ data/download_skempi_v2.sh
@@ -1,5 +1,5 @@
-mkdir -p SKEMPI_v2
-cd SKEMPI_v2
+mkdir -p skempi_pdbs
+cd skempi_pdbs
 wget https://life.bsc.es/pid/skempi2/database/download/skempi_v2.csv
 wget https://life.bsc.es/pid/skempi2/database/download/SKEMPI2_PDBs.tgz
 tar -xzvf SKEMPI2_PDBs.tgz
